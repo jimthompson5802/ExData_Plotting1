45d616f66f31b3f52d3215ca391d38e318f87dee
Jim Thompson <jimthompson5802@aol.com>
2014-05-06 00:03:11 -0400

    Initial baseline for Plot 1

--- Plot1.R
+++ Plot1.R
@@ -1,3 +1,32 @@
 ###
 # Plot 1
 ###
+
+## read in power consumption data file
+hpc <- read.csv("./household_power_consumption.txt",sep=";",
+                 colClasses=c("character","character",rep("numeric",7)),
+                na.strings="?")
+
+## quick overview of data
+cat("number of rows & columns:\n",dim(hpc))
+cat("column names:\n",names(hpc))
+cat("Overall structure:\n",str(hpc))
+
+## extract only data for 2/1/2007 and 2/2/2007
+hpc <- hpc[as.Date(hpc$Date,"%d/%m/%Y") == as.Date("2007-2-1") |
+           as.Date(hpc$Date,"%d/%m/%Y") == as.Date("2007-2-2"),]
+
+
+## create vector of date/time stamps for selecting required date range
+hpc$date.time <- strptime(paste(hpc$Date,hpc$Time,sep=" "),"%d/%m/%Y %H:%M:%S")
+str(hpc)
+
+## generate Plot 1
+hist(hpc$Global_active_power,col="red",
+     main="Global Active Power",
+     xlab="Global Active Power (kilowatts)")
+
+## copy to png file
+dev.copy(png,file="Plot1.png")
+dev.off()
+                            
